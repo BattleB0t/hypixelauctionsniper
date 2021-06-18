0388e03974c1f8f3a12dc90a22fa87579c014198
Heorty <60544342+Heorty@users.noreply.github.com>
2021-06-18 13:18:31 +0200

    check des profits pour chaques items simplement

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const HypixelAPI = require('hypixel-api');
 
 const nbt = require('prismarine-nbt');
 const util = require('util');
-const { parse } = require('path');
-const { stringify } = require('querystring');
+const {
+    parse
+} = require('path');
+const {
+    stringify
+} = require('querystring');
 
 const nbtUtil = require('./nbtUtil.js')
+const ahPage = require('./ahPage.json')
+const lowestBin = require('./lowestBin.json')
+
+
 
 const client = new HypixelAPI();
 
@@ -18,12 +26,12 @@ async function search() {
         item = await client.getSkyblockAuctions(j);
         item.auctions.forEach(element => {
             if (element.bin && !element.claimed) {
-                nbtUtil.decodeData(element.item_bytes).then(nbt => { 
+                nbtUtil.decodeData(element.item_bytes).then(nbt => {
 
                     nbtUtil.nbtToSbID(nbt).then(sbID => {
                         // comparaison des prix ici
- 
-                    })                
+
+                    })
                 })
             }
 
@@ -33,6 +41,34 @@ async function search() {
 
 }
 
-search()
+async function searchWoAPI() {
+    ahPage.auctions.forEach(element => {
+        if (element.bin && !element.claimed) {
+            nbtUtil.decodeData(element.item_bytes).then(nbt => {
+
+                nbtUtil.nbtToSbID(nbt).then(sbID => {
+                    nbtUtil.decodeData(element.item_bytes).then(itemNbt => {
+                        nbtUtil.nbtToSbID(itemNbt).then(resp => {
 
 
+                            if ((lowestBin[resp]) - (element.starting_bid) > 100000){
+
+                                console.log(element.item_name)
+                                console.log(`profit : ${(lowestBin[resp]) - (element.starting_bid)}`)
+                                console.log('')
+                                
+                            }
+
+                            
+
+                        })
+                    })
+
+                })
+            })
+        }
+
+    });
+}
+
+searchWoAPI()
